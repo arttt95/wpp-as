8aef4d2f2ff7306a843c533629080b9eea8b02da
Arthur Damasceno <thuurdamasceno@gmail.com>
2025-02-02 02:09:21 -0300

    02FEV25 - Aula 262 AS - Criando a toolbar customizada.

--- app/src/main/java/com/arttt95/whatsapp/LoginActivity.kt
+++ app/src/main/java/com/arttt95/whatsapp/LoginActivity.kt
@@ -1,20 +1,41 @@
 package com.arttt95.whatsapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.arttt95.whatsapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        inicializarEventosClique()
+
+    }
+
+    private fun inicializarEventosClique() {
+
+        binding.textCadastro.setOnClickListener {
+            startActivity(
+                Intent(this, CadastroActivity::class.java)
+            )
+        }
+
     }
 }
