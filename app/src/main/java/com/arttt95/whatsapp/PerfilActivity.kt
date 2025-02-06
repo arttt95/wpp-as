05db4c01f48866d36c647bb7de3514523bd08b42
Arthur Damasceno <thuurdamasceno@gmail.com>
2025-02-06 00:18:45 -0300

    06FEV25 - Aula 269 AS - Criando interface de perfil.

--- app/src/main/java/com/arttt95/whatsapp/PerfilActivity.kt
+++ app/src/main/java/com/arttt95/whatsapp/PerfilActivity.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.arttt95.whatsapp.databinding.ActivityPerfilBinding
 
 class PerfilActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityPerfilBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_perfil)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        inicializarToolbar()
+
     }
+
+    private fun inicializarToolbar() {
+        val toolbar = binding.includeToolbarPerfil.tbPrincipal
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = "Perfil"
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
 }
